1d28b05dd5d54c4b55a76d6ee9a3a8a0256b6dcf
IrynaMileiko <irencraib@gmail.com>
2021-10-23 17:15:24 +0300

    Login FE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserpanelComponent } from './components/userpanel/userpanel.component';
 import { LotpanelComponent } from './components/lotpanel/lotpanel.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const appRoutes:Routes = [
   {path:'', component:HomeComponent},//ссылка и какой компонент отображает
   {path:'userpanel', component:UserpanelComponent},
-  {path:'lotpanel', component:LotpanelComponent}
+  {path:'lotpanel', component:LotpanelComponent},
+  {path:'login', component:LoginComponent},
+  {path:'register', component:RegisterComponent}
 ]
 
 @NgModule({
@@ -20,7 +25,9 @@ const appRoutes:Routes = [
     AppComponent,
     HomeComponent,
     UserpanelComponent,
-    LotpanelComponent
+    LotpanelComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
