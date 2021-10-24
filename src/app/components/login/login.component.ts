20b7ee20319f66abaf733941a3eef0266d1bb4e4
IrynaMileiko <irencraib@gmail.com>
2021-10-24 20:13:25 +0300

    Registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,14 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  login:string;
+  password:string;
+  loginMsg:string;
+  passwordMsg:string;
+  invalidLogin:boolean;
+  invalidPassword:boolean;
 
-  constructor() { }
+  constructor() {
+    this.login='';
+    this.password='';
+    this.loginMsg='';
+    this.passwordMsg='';
+    this.invalidPassword=false;
+    this.invalidLogin=false;
+   }
 
   ngOnInit(): void {
   }
 
 
-  register(name:HTMLInputElement, password:HTMLInputElement){
-    name.setCustomValidity('Hello');
+  loginF(){
+    this.login=this.login.trim();
+    this.password=this.password.trim();
+    this.validate();
   }
+
+  validate(){
+    return(this.validateLogin()&&this.validatePassword());
+  }
+
+  validateLogin(){
+    if(this.login.length==0){
+      this.invalidLogin=true;
+      this.loginMsg="This field is required";
+      return false;
+    }
+
+    return true;
+  }
+
+  validatePassword(){
+    if(this.password.length==0){
+      this.invalidPassword=true;
+      this.passwordMsg="This field is required";
+      return false;
+    }
+
+    return true;
+  }
+
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,10 +6,185 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  regEName = new RegExp("^([A-ZА-Я][a-zа-я]+[-.']*[A-ZА-Я]*[a-zа-я]+)$");
+  regESpace = new RegExp("^(.* .*)$");
+  regEEmail = new RegExp("^([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+)$");
+  regEPhone = new RegExp("^\\+\\d{7,19}$");
 
-  constructor() { }
+  login:string;
+  password:string;
+  email:string;
+  phone:string;
+  firstName:string;
+  lastName:string;
+  confPassword:string;
+
+  loginMsg:string;
+  passwordMsg:string;
+  emailMsg:string;
+  phoneMsg:string;
+  firstNameMsg:string;
+  lastNameMsg:string;
+  confPasswordMsg:string;
+
+  invalidLogin:boolean;
+  invalidPassword:boolean;
+  invalidEmail:boolean;
+  invalidPhone:boolean;
+  invalidFirstName:boolean;
+  invalidLastName:boolean;
+  invalidConfPassword:boolean;
+
+  constructor() {
+  this.login='';
+  this.password='';
+  this.email='';
+  this.phone='';
+  this.firstName='';
+  this.lastName='';
+  this.confPassword='';
+
+  this.loginMsg='';
+  this.passwordMsg='';
+  this.emailMsg='';
+  this.phoneMsg='';
+  this.firstNameMsg='';
+  this.lastNameMsg='';
+  this.confPasswordMsg='';
+
+  this.invalidPassword=false;
+  this.invalidLogin=false;
+  this.invalidEmail=false;
+  this.invalidPhone=false;
+  this.invalidFirstName=false;
+  this.invalidLastName=false;
+  this.invalidConfPassword=false;
+ }
 
   ngOnInit(): void {
+  }
+
+
+register(){
+  this.login=this.login.trim();
+  this.password=this.password.trim();
+  this.email=this.email.trim();
+  this.phone=this.phone.trim();
+  this.firstName=this.firstName.trim();
+  this.lastName=this.lastName.trim();
+  this.confPassword=this.confPassword.trim();
+  this.validate();
+}
+
+
+  validate(){
+    let goodLogin=this.validateLogin();
+    let goodPassword=this.validatePassword();
+    let goodEmail=this.validateEmail();
+    let goodPhone=this.validatePhone();
+    let goodFN=this.validateFirstName();
+    let goodLN=this.validateLastName();
+    let goodCP=this.validateConfirmPassword();
+  }
+
+  validateLogin(){
+    this.invalidLogin=false;
+    if(this.login.length==0){
+      this.invalidLogin=true;
+      this.loginMsg="This field is required";
+    }
+    if(this.login.search(this.regESpace)!=-1){
+      this.invalidLogin=true;
+      this.loginMsg="Login can't contain spaces";
+      return false;
+    }
+
+    return true;
+  }
+  validatePassword(){
+    this.invalidPassword=false;
+    if(this.password.length==0){
+      this.invalidPassword=true;
+      this.passwordMsg="This field is required";
+      return false;
+    }
+    if(this.password.search(this.regESpace)!=-1){
+      this.invalidPassword=true;
+      this.passwordMsg="Password can't contain spaces";
+      return false;
+    }
+
+    return true;
+  }
+  validateEmail(){
+    this.invalidEmail=false;
+    if(this.email.length==0){
+      this.invalidEmail=true;
+      this.emailMsg="This field is required";
+      return false;
+    }
+    if(this.email.search(this.regEEmail)==-1){
+      this.invalidEmail=true;
+      this.emailMsg="Invalid format";
+      return false;
+    }
+
+    return true;
+  }
+  validatePhone(){
+    this.invalidPhone=false;
+    if(this.phone.length==0){
+      this.invalidPhone=true;
+      this.phoneMsg="This field is required";
+      return false;
+    }
+    if(this.phone.search(this.regEPhone)==-1){
+      this.invalidPhone=true;
+      this.phoneMsg="Phone number should starts with '+' and have 7-19 digits";
+      return false;
+    }
+
+    return true;
+  }
+  validateFirstName(){
+    this.invalidFirstName=false;
+    if(this.firstName.length==0){
+      this.invalidFirstName=true;
+      this.firstNameMsg="This field is required";
+      return false;
+    }
+    if(this.firstName.search(this.regEName)==-1){
+      this.invalidFirstName=true;
+      this.firstNameMsg="Invalid format";
+      return false;
+    }
+
+    return true;
+  }
+  validateLastName(){
+    this.invalidLastName=false;
+    if(this.lastName.length==0){
+      this.invalidLastName=true;
+      this.lastNameMsg="This field is required";
+      return false;
+    }
+    if(this.lastName.search(this.regEName)==-1){
+      this.invalidLastName=true;
+      this.lastNameMsg="Invalid format";
+      return false;
+    }
+
+    return true;
+  }
+  validateConfirmPassword(){
+    this.invalidConfPassword=false;
+    if(this.confPassword!=this.password){
+      this.invalidConfPassword=true;
+      this.confPasswordMsg="Passwords don't match";
+      return false;
+    }
+
+    return true;
   }
 
 }
