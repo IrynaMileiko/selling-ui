54933757025d7027c04f6803a8420973db2cc7a9
IrynaMileiko <irencraib@gmail.com>
2021-10-28 20:45:58 +0300

    registr try

--- src/app/services/hello.service.ts
+++ src/app/services/hello.service.ts
@@ -7,11 +7,43 @@ import { HttpClient } from '@angular/common/http';
 export class HelloService {
 
 private url='http://localhost:8081/api/user/hello';
+user:User;
 
-  constructor(private http: HttpClient) { }
+ngOnInit(){
+  this.user={
+    email:'someEmail@gmail.com',
+    firstName:'Andrew',
+    lastName:'McLagen',
+    password:'veryCoolPSW123'
+  };
+}
+
+
+  constructor(private http: HttpClient) {
+    this.user={
+      email:'someEmail@gmail.com',
+      firstName:'Andrew',
+      lastName:'McLagen',
+      password:'veryCoolPSW123'
+    };
+   }
 
   getHello() {
   	return this.http.get(this.url,
       {responseType:'text'});
   }
+    tryRegister() {
+    let url='http://localhost:8081/api/v1/registration/registrationUser';
+    	return this.http.post(url, this.user,
+        {responseType:'text'});
+    }
+
+
+}
+
+interface User{
+  email: String;
+  firstName:String;
+  lastName:String;
+  password:String;
 }
